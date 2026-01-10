65b03303007abb9b4a42c2190e347ab6c6524880
mcuw <112967559+mcuw@users.noreply.github.com>
2026-01-10 14:01:09 +0100

    chore pinouts

--- include/pinout.h
+++ include/pinout.h
@@ -1,18 +1,18 @@
 #ifndef PINOUT_H
 #define PINOUT_H
 
-#define LED 13
-#define BUTTON 0
+#define LED GPIO_NUM_13
+#define BUTTON GPIO_NUM_0
 
 // Relay Module Pins
-#define RELAY_LED_1 27
-#define RELAY_LED_2 14
-#define RELAY_SWITCH_1 32
-#define RELAY_SWITCH_2 33
+#define RELAY_LED_1 GPIO_NUM_27
+#define RELAY_LED_2 GPIO_NUM_14
+#define RELAY_SWITCH_1 GPIO_NUM_32
+#define RELAY_SWITCH_2 GPIO_NUM_33
 
 // Power Sensor Pins
 // CSE7761 sensor (v1.x), while later versions use the BL0939 sensor (v2.x)
-#define POWER_SENSOR_TX 25
-#define POWER_SENSOR_RX 26
+#define POWER_SENSOR_TX GPIO_NUM_25
+#define POWER_SENSOR_RX GPIO_NUM_26
 
 #endif
