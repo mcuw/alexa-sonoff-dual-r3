5f32aca747c4268ebabe9745108f67fbba0bac07
mcuw <112967559+mcuw@users.noreply.github.com>
2026-01-10 13:56:03 +0100

    extend pinout.h

--- include/pinout.h
+++ include/pinout.h
@@ -1,1 +1,18 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
 #define LED 13
+#define BUTTON 0
+
+// Relay Module Pins
+#define RELAY_LED_1 27
+#define RELAY_LED_2 14
+#define RELAY_SWITCH_1 32
+#define RELAY_SWITCH_2 33
+
+// Power Sensor Pins
+// CSE7761 sensor (v1.x), while later versions use the BL0939 sensor (v2.x)
+#define POWER_SENSOR_TX 25
+#define POWER_SENSOR_RX 26
+
+#endif
